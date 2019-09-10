7a965809b08c8e69ed2a7026fbaceefc2842a470
Usong Cho <silvermouse4012@gmail.com>
2019-09-10 16:42:20 -0700

    added Search contorller

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,24 @@ import Search from './models/Search';
  */
 const state = {};
 
-const search = new Search('pizza');
-search.getResults();
+const controlSearch = async () => {
+    //Get query from the view
+    const query = 'pizza';
+
+    if (query) {
+        //new search object and added to state;
+        state.search = new Search(query);
+
+        //prepare UI for the results
+
+        //search for recipes
+        await state.search.getResults();
+
+        //render results to the UI
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
